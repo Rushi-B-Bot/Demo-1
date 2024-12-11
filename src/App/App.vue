12cc17be4a3783267189b5dcea91799c14bebb12
rushibhondave <rushikeshbhondave99@gmail.com>
2024-12-11 14:13:47 +0530

    All Done:

---
+++ src/App/App.vue
@@ -0,0 +1,22 @@
+
+<script>
+import MasterBranch from '../components/MasterBranch'
+
+export default {
+  name: 'App',
+  components: {
+    MasterBranch,
+  }
+}
+</script>
+
+
+<template>
+
+<MasterBranch/>
+
+</template>
+
+<style>
+
+</style>
---
+++ src/components/MasterBranch.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+
+}
+</script>
+
+
+<template>
+  <div>
+    <h1>Hi thi is MasterBranch</h1>
+  </div>
+</template>
+
+<style scoped></style>
